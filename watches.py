e3c59cd4e5ed4c0cbbf6ba64fda99f3c3a2e7b74
Jeff Kunzelman <jeff@river.io>
2015-11-16 21:59:16 -0700

    add magnetometer and orientation

--- watches.py
+++ watches.py
@@ -10,10 +10,14 @@ while True:
     temp = sense.get_temperature()
     humidity = sense.get_humidity()
     pressure = sense.get_pressure()
+    orientation = sense.get_orientation_degrees()
+    rawMag = sense.get_compass_raw()
 
     print("Temperature: %s C" % temp)
     print("Humidity: %s %%rH" % humidity)
     print("Pressure: %s Millibars" % pressure)
+    print("Magnetometer x: {x}, y: {y}, z: {z}".format(**rawMag))
+    print("Orientation: p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
 
     pixels = [red if i < temp else blue for i in range(64)]
 
