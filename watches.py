c807c9b585d1d913af63f18f0f91b6a1854e33ff
Jeff Kunzelman <jeff@softwareops.com>
2015-11-17 21:48:43 -0700

    add log formatting

--- watches.py
+++ watches.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from sense_hat import SenseHat
 
 sense = SenseHat()
@@ -12,6 +13,7 @@ while True:
     pressure = sense.get_pressure()
     orientation = sense.get_orientation_degrees()
     rawMag = sense.get_compass_raw()
+    timestamp = time.ctime()
 
     print("____________________________________________________________________")
     print("Temperature: %s C" % temp)
@@ -19,6 +21,9 @@ while True:
     print("Pressure: %s Millibars" % pressure)
     print("Magnetometer x: {x}, y: {y}, z: {z}".format(**rawMag))
     print("Orientation: p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
+    
+    log_record = "%s temp=%s humidity=%s pressure=%s" (timestamp,temp,humidity,pressure)
+    print(log_record)
 
     pixels = [red if i < temp else blue for i in range(64)]
 
