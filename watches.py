2f7abbaefbc63c1c4c84a5fc897e52bfdd4e44cd
jkunzelman <jeff@river.io>
2016-01-08 20:25:46 -0700

    aadd gyro logging

--- watches.py
+++ watches.py
@@ -11,18 +11,23 @@ while True:
     temp = sense.get_temperature()
     humidity = sense.get_humidity()
     pressure = sense.get_pressure()
-    orientation = sense.get_orientation_degrees()
+    gyro = sense.get_gyroscope()
     rawMag = sense.get_compass_raw()
     timestamp = time.ctime()
+    accel_only = sense.get_accelerometer()
+    north = sense.get_compass()
 
     print("____________________________________________________________________")
     print("Temperature: %s C" % temp)
     print("Humidity: %s %%rH" % humidity)
+  
     print("Pressure: %s Millibars" % pressure)
+    print("North: %s" % north)
     print("Magnetometer x: {x}, y: {y}, z: {z}".format(**rawMag))
-    print("Orientation: p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
+    print("Gyro: p: {pitch}, r: {roll}, y: {yaw}".format(**gyro))
+    print("Accel: p: {pitch}, r: {roll}, y: {yaw}".format(**accel_only))
     
-    log_record = "%s temp=%s humidity=%s pressure=%s" % (timestamp,temp,humidity,pressure)
+    log_record = "%s : temp=%s humidity=%s pressure=%s gyro=%s accel=%s mag=%s" % (timestamp,temp,humidity,pressure,sense.gyro,sense.accel,sense.compass_raw)
     print(log_record)
     
     pixels = [red if i < temp else blue for i in range(64)]
